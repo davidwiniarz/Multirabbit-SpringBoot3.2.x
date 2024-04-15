96aa183bd45c32a64611b2cdef4184967506f481
daw-s@wp.pl <daw-s@wp.pl>
2024-04-15 14:29:22 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/AmqpConfig.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+class AmqpConfig implements BeanFactoryAware {
+
+    private boolean enabled;
+    private BeanFactory beanFactory;
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.beanFactory = beanFactory;
+    }
+
+    @Bean
+    MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter());
+        return rabbitTemplate;
+    }
+
+}
---
+++ src/main/java/com/example/demo/CasesRabbitListener.java
@@ -0,0 +1,52 @@
+package com.example.demo;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+
+@Component
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+class CasesRabbitListener {
+
+
+    @RabbitListener(
+            id = "dev",
+            autoStartup = "true",
+            priority = "100",
+            ackMode = "MANUAL"
+    )
+    Mono<Void> listenDev(final Message message) {
+        return null;
+    }
+
+    @RabbitListener(
+            id = "dev2",
+            autoStartup = "true",
+            priority = "100",
+            containerFactory = "dev2",
+            ackMode = "MANUAL"
+
+    )
+    Mono<Void> listenDev2(final Message message) {
+        return null;
+    }
+
+
+    @RabbitListener(
+            id = "dev3",
+            autoStartup = "true",
+            priority = "100",
+            containerFactory = "dev3",
+            ackMode = "MANUAL"
+
+    )
+    Mono<Void> listenDev3(final Message message) {
+        return null;
+    }
+
+
+}
