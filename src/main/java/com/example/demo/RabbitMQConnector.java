b06d22a8732e0255e53f360520875ec6ec777388
daw-s@wp.pl <daw-s@wp.pl>
2024-04-15 14:30:17 +0200

    Get rid of MQ Connector, but without it, queues are not connecting

--- src/main/java/com/example/demo/RabbitMQConnector.java
+++ src/main/java/com/example/demo/RabbitMQConnector.java
@@ -1,15 +1,15 @@
-package com.example.demo;
-
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
-import org.springframework.boot.autoconfigure.amqp.MultiRabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
-@ConditionalOnProperty(prefix = "spring", name = "multirabbitmq.enabled", havingValue = "true")
-@Import({MultiRabbitAutoConfiguration.class, RabbitAutoConfiguration.class})
-@Configuration
-@EnableRabbit
-public class RabbitMQConnector {
-}
+//package com.example.demo;
+//
+//import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+//import org.springframework.boot.autoconfigure.amqp.MultiRabbitAutoConfiguration;
+//import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Import;
+//
+//@ConditionalOnProperty(prefix = "spring", name = "multirabbitmq.enabled", havingValue = "true")
+//@Import({MultiRabbitAutoConfiguration.class, RabbitAutoConfiguration.class})
+//@Configuration
+//@EnableRabbit
+//public class RabbitMQConnector {
+//}
