399ae2f5913e1a5eceb21415da8201b5207b3cd5
daw-s@wp.pl <daw-s@wp.pl>
2024-04-16 10:24:38 +0200

    Listener change

--- src/main/java/com/example/demo/CasesRabbitListener.java
+++ src/main/java/com/example/demo/CasesRabbitListener.java
@@ -2,38 +2,41 @@ package com.example.demo;
 
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
-import reactor.core.publisher.Mono;
 
 
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Slf4j
 class CasesRabbitListener {
 
 
     @RabbitListener(
             id = "dev",
             autoStartup = "true",
-            priority = "100",
-            ackMode = "MANUAL",
             queues = "dw_q_1"
     )
-    Mono<Void> listenDev(final Message message) {
-        return null;
+    void listenDev(final Message message) throws JSONException {
+        var stringBody = new String(message.getBody());
+        var jsonBody = new JSONObject(stringBody);
+        log.info("1:" + jsonBody.get("key"));
     }
 
     @RabbitListener(
             id = "dev2",
             autoStartup = "true",
-            priority = "100",
             containerFactory = "dev2",
-            ackMode = "MANUAL",
             queues = "dw_q_2"
     )
-    Mono<Void> listenDev2(final Message message) {
-        return null;
+    void listenDev2(final Message message) throws JSONException {
+        var stringBody = new String(message.getBody());
+        var jsonBody = new JSONObject(stringBody);
+        log.info("2:" + jsonBody.get("key"));
     }
 
 
